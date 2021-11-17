fa62ec17f7fd61574de6add80ce9f9eee7f4c9de
KhueHM <khuehm@outlook.com>
2021-11-17 16:29:53 +0700

    BMX160 works!

---
+++ localization/bmx160/src/main.cpp
@@ -0,0 +1,71 @@
+#include <DFRobot_BMX160.h>
+
+DFRobot_BMX160 bmx160;
+
+void setup(){
+  Serial.begin(115200);
+  delay(100);
+  
+  //init the hardware bmx160  
+  if (bmx160.begin() != true){
+    Serial.println("init false");
+    while(1);
+  }
+  //bmx160.setLowPower();   //disable the gyroscope and accelerometer sensor
+  //bmx160.wakeUp();        //enable the gyroscope and accelerometer sensor
+  //bmx160.softReset();     //reset the sensor
+  
+  /** 
+   * enum{eGyroRange_2000DPS,
+   *       eGyroRange_1000DPS,
+   *       eGyroRange_500DPS,
+   *       eGyroRange_250DPS,
+   *       eGyroRange_125DPS
+   *       }eGyroRange_t;
+   **/
+  //bmx160.setGyroRange(eGyroRange_500DPS);
+  
+  /**
+   *  enum{eAccelRange_2G,
+   *       eAccelRange_4G,
+   *       eAccelRange_8G,
+   *       eAccelRange_16G
+   *       }eAccelRange_t;
+   */
+  //bmx160.setAccelRange(eAccelRange_4G);
+  delay(100);
+}
+
+void loop(){
+  sBmx160SensorData_t Omagn, Ogyro, Oaccel;
+
+  /* Get a new sensor event */
+  bmx160.getAllData(&Omagn, &Ogyro, &Oaccel);
+  Serial.print(Omagn.x); Serial.print(",");
+  Serial.print(Omagn.y); Serial.print(",");
+  Serial.print(Omagn.z);
+  /* Display the magnetometer results (magn is magnetometer in uTesla) */
+  // Serial.print("M ");
+  // Serial.print("X: "); Serial.print(Omagn.x); Serial.print("  ");
+  // Serial.print("Y: "); Serial.print(Omagn.y); Serial.print("  ");
+  // Serial.print("Z: "); Serial.print(Omagn.z); Serial.print("  ");
+  // Serial.println("uT");
+
+  // /* Display the gyroscope results (gyroscope data is in g) */
+  // Serial.print("G ");
+  // Serial.print("X: "); Serial.print(Ogyro.x); Serial.print("  ");
+  // Serial.print("Y: "); Serial.print(Ogyro.y); Serial.print("  ");
+  // Serial.print("Z: "); Serial.print(Ogyro.z); Serial.print("  ");
+  // Serial.println("g");
+  
+  // /* Display the accelerometer results (accelerometer data is in m/s^2) */
+  // Serial.print("A ");
+  // Serial.print("X: "); Serial.print(Oaccel.x    ); Serial.print("  ");
+  // Serial.print("Y: "); Serial.print(Oaccel.y    ); Serial.print("  ");
+  // Serial.print("Z: "); Serial.print(Oaccel.z    ); Serial.print("  ");
+  // Serial.println("m/s^2");
+
+  Serial.println("");
+
+  delay(50);
+}
