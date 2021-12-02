ac65abb03a88d8915311bec305349de0510c24f4
KhueHM <khuehm@outlook.com>
2021-12-02 19:13:54 +0700

    negligible changes

--- stereo-vision/stereo_depth.py
+++ stereo-vision/stereo_depth.py
@@ -17,18 +17,18 @@ def depth_map(imgL, imgR):
     window_size = 3
 
     left_matcher = cv2.StereoSGBM_create(
-        minDisparity=-1,
-        numDisparities=5*16,  # max_disp has to be dividable by 16 f. E. HH 192, 256
-        blockSize=window_size,
-        P1=8 * 3 * window_size,
+        minDisparity = -1,
+        numDisparities = 5 * 16,  # max_disp has to be dividable by 16 f. E. HH 192, 256
+        blockSize = window_size,
+        P1 = 8 * 3 * window_size,
         # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
-        P2=32 * 3 * window_size,
-        disp12MaxDiff=12,
-        uniquenessRatio=10,
-        speckleWindowSize=50,
-        speckleRange=32,
-        preFilterCap=63,
-        mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
+        P2= 32 * 3 * window_size,
+        disp12MaxDiff = 12,
+        uniquenessRatio = 10,
+        speckleWindowSize = 50,
+        speckleRange = 32,
+        preFilterCap = 63,
+        mode = cv2.STEREO_SGBM_MODE_SGBM_3WAY
     )
     right_matcher = cv2.ximgproc.createRightMatcher(left_matcher)
     # FILTER Parameters
