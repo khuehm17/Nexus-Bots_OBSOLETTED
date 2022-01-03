0ff47b260c39a5e3f99799ddcc65e45787bc2fd5
khuehm17 <khuehm@outlook.com>
2022-01-03 16:07:43 +0700

    add test_pkg to ros workspace

---
+++ nexus-ros/raspi3b/raspi3b_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/works/Nexus-Bots/nexus-ros/raspi3b/raspi3b_ws/src
+# Build directory: /home/ubuntu/works/Nexus-Bots/nexus-ros/raspi3b/raspi3b_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(test_pkg)
