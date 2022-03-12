1ecc629f773acd29d1eb9659fea740464fc0f319
HaoPC <conghao.cha2009@gmail.com>
2022-03-12 14:00:32 +0700

    modify nexus-base/program/

--- nexus-base/program/nexus-bots-pio/include/main.h
+++ nexus-base/program/nexus-bots-pio/include/main.h
@@ -7,6 +7,11 @@
 #define MICRO_PER_SEC 1000000
 #endif
 
+#define BODY_RADIUS                     110 //mm
+#define REDUCTION_RATIO_NAMIKI_MOTOR    80
+#define WHEEL_RADIUS                    24
+#define WHEEL_CIRC                      (WHEEL_RADIUS * 2 * M_PI)
+
 // Motor 1
 #define M1_PWM    5
 #define M1_DIR    4
@@ -26,8 +31,8 @@
 irqISR(irq1, isr1); // This will create a MotorWheel object called Wheel1
 irqISR(irq2, isr2); // This will create a MotorWheel object called Wheel2
 irqISR(irq3, isr3); // This will create a MotorWheel object called Wheel3
-MotorWheel wheel1(M1_PWM, M1_DIR, M1_ENCA, M1_ENCB, &irq1);
-MotorWheel wheel2(M2_PWM, M2_DIR, M2_ENCA, M2_ENCB, &irq2);
-MotorWheel wheel3(M3_PWM, M3_DIR, M3_ENCA, M3_ENCB, &irq3);
+MotorWheel wheel1(M1_PWM, M1_DIR, M1_ENCA, M1_ENCB, &irq1, REDUCTION_RATIO_NAMIKI_MOTOR, WHEEL_CIRC);
+MotorWheel wheel2(M2_PWM, M2_DIR, M2_ENCA, M2_ENCB, &irq2, REDUCTION_RATIO_NAMIKI_MOTOR, WHEEL_CIRC);
+MotorWheel wheel3(M3_PWM, M3_DIR, M3_ENCA, M3_ENCB, &irq3, REDUCTION_RATIO_NAMIKI_MOTOR, WHEEL_CIRC);
 
 #endif
