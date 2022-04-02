a370706a3cc9627280845af3d28f392749bb8165
KhoiVTM <khoivtm@example.com>
2022-04-02 10:16:32 +0700

    release auto captured

--- nexus-vision/Depthmap_Dev/get_calib_img_ver02.py
+++ nexus-vision/Depthmap_Dev/get_calib_img_ver02.py
@@ -19,7 +19,7 @@ countdown = 5                 # Interval for count-down timer, seconds
 font=cv2.FONT_HERSHEY_SIMPLEX # Cowntdown timer font
 count_img = 0                 # Count number of images   
 
-output_dir = "./data/calib_img"
+output_dir = "./nexus-vision/data/calib_img"
 prefix = "img"
 left_prefix = 'left_cam_'
 right_prefix = 'right_cam_'
@@ -42,14 +42,14 @@ while(True):
     rigthCam = orginal[0:F_HEIGHT, 0:int(F_WIDTH/2)]
     leftCam = orginal[0:F_HEIGHT, int(F_WIDTH/2):F_WIDTH]
 
-    cv2.imshow('Left CAM', leftCam)
-    cv2.imshow('Right CAM', rigthCam)
+    #cv2.imshow('Left CAM', leftCam)
+    #cv2.imshow('Right CAM', rigthCam)
     
     # TODO: use os.path.join() method here
     right_img_path = output_dir + '/' + right_prefix + str(count_img) + '.' + image_format
     left_img_path = output_dir + '/' + left_prefix + str(count_img) + '.' + image_format
     print(cntdwn_timer)
-    if cntdwn_timer == -6:
+    if cntdwn_timer == -4:
         count_img += 1
 
         cv2.imwrite(right_img_path, rigthCam)
@@ -60,9 +60,11 @@ while(True):
         time.sleep(1)
         cntdwn_timer = 0      # To avoid "-1" timer display 
         next
-        
     #Draw countdown counter
     cv2.putText(leftCam, str(cntdwn_timer), (50,50), font, 2.0, (0,0,255),4, cv2.LINE_AA)
+    cv2.imshow('Left CAM', leftCam)
+    cv2.imshow('Right CAM', rigthCam)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
